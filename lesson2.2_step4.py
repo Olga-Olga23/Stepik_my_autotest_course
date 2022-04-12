029ac499a5f04700d9813459129ec576af45a8a4
Olga-Olga23 <9213713241@mail.ru>
2022-04-12 13:03:06 +0300

    Lesson 2.2

---
+++ lesson2.2_step4.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+import math
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+browser = webdriver.Chrome()
+link = "http://SunInJuly.github.io/execute_script.html"
+browser.get(link)
+
+x_element = browser.find_element_by_css_selector('#input_value')
+x = x_element.text
+y = calc(x)
+
+button = browser.find_element_by_tag_name("button")
+browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+button.click()
+
+try: 
+   
+    # Ваш код, который заполняет обязательные поля
+    
+    input1 = browser.find_element_by_id('answer')
+    input1.send_keys(calc(x))
+    
+    option1 = browser.find_element_by_css_selector("[type='checkbox']")
+    option1.click()
+
+    option1 = browser.find_element_by_css_selector("[value='robots']")
+    option1.click()
+    
+    # Отправляем заполненную форму
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+
+
